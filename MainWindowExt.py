ffb4e6283fc928f4b0791c2131b63fd55d8eb003
Anna PhuocThinh <thinhlp23416@st.uel.edu.vn>
2024-11-11 21:20:38 +0700

    Them try except

--- MainWindowExt.py
+++ MainWindowExt.py
@@ -14,27 +14,31 @@ class MainWindowExt(Ui_MainWindow):
         self.MainWindow.show()
 
     def update(self):
-        a = float(self.lineEditA.text())
-        b = float(self.lineEditB.text())
-        c = float(self.lineEditC.text())
+        try:
+            a = float(self.lineEditA.text())
+            b = float(self.lineEditB.text())
+            c = float(self.lineEditC.text())
 
-        delta = float(b * b - 4 * a * c)
-        if (a == 0):
-            if (b == 0 and c != 0):
-                x1 = x2 = "Vo nghiem"
-            elif (b == 0 and c == 0):
-                x1 = x2 = "Vo so nghiem"
+            delta = float(b * b - 4 * a * c)
+            if (a == 0):
+                if (b == 0 and c != 0):
+                    x1 = x2 = "Vo nghiem"
+                elif (b == 0 and c == 0):
+                    x1 = x2 = "Vo so nghiem"
+                else:
+                    x = float(-c / b)
+                    x1 = x2 = x
             else:
-                x = float(-c / b)
-                x1 = x2 = x
-        else:
-            if (delta < 0):
-                x1 = x2 = "Vo nghiem"
-            elif (delta == 0):
-                x1 = x2 = float(-b / 2 * a)
-            else:
-                x1 = float((-b + math.sqrt(delta)) / (2 * a))
-                x2 = float((-b - math.sqrt(delta)) / (2 * a))
+                if (delta < 0):
+                    x1 = x2 = "Vo nghiem"
+                elif (delta == 0):
+                    x1 = x2 = float(-b / 2 * a)
+                else:
+                    x1 = float((-b + math.sqrt(delta)) / (2 * a))
+                    x2 = float((-b - math.sqrt(delta)) / (2 * a))
 
-        self.lineEditX1.setText(f"{x1}")
-        self.lineEditX2.setText(f"{x2}")
+            self.lineEditX1.setText(f"{x1}")
+            self.lineEditX2.setText(f"{x2}")
+
+        except:
+            pass
